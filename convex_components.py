6c6f3fce4465ebc26f596ce66dafdabef079a7d9
ivan-paz <ivnpaz@gmail.com>
2017-03-08 23:32:01 +0100

    intersection

--- convex_components.py
+++ convex_components.py
@@ -23,8 +23,14 @@ def select_random_rule(R):
 #...............................................................
 #Given a rule r and a rule base R
 #search for intersections between R and r
-def search_intersections(rule,R):
-	print(rule)
+def search_intersections(R):
+    [rule,R] = select_random_rule(R)
+    print(rule,R)
+    
+search_intersections(R)
+
+
+
 #...............................................................
 #Given two rules return False if they have the same class
 def sameClass(rule1,rule2):
@@ -41,16 +47,35 @@ def min_max(element):
 		minimum = element
 		maximum = element
 	else:
-		mimimum = min(element)
+		minimum = min(element)
 		maximum = max(element)
 	return (minimum,maximum)
 	#print(min_max(3))
+#min_max((1,3,6))
+
+#Check if two sets intersect
+def interval_intersection(int1, int2):
+    if ( (int1[0]<= int2[0] <= int1[1]) or (int1[0]<= int2[1] <= int1[1]) ):
+        return True
+interval_intersection( (1,11), (9,12) )
+
 #Given two rules Returns true if they intersect each other
 def intersect(rule1, rule2):
-#	if sameClass(rule1,rule2) == False:
-	#For each set S calculate the minimum and maximum
-#		for i in range(len(rule1) - 1):
-#			print(min_max(rule1[i]))
-#			print(min_max(rule2[i]))
-			
+    if sameClass(rule1,rule2) == False:
+        #For each set S calculate the minimum and maximum
+        for i in range(len(rule1) - 1):
+            #print(min_max(rule1[i]))
+            #print(min_max(rule2[i]))
+        #Check if if Sets intersect each other
+            return interval_intersection( min_max(rule1[i]), min_max(rule2[i]) )
+        
 #intersect(( (1, 2, 3, 8, 11), (4, 6), 'A'),  ( (9,12),            5,     'C'))
+
+
+
+
+
+
+
+
+
