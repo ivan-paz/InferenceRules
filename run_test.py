a298a580e405386a17ab23c5358f6aaa79272feb
ivan-paz <ivnpaz@gmail.com>
2017-03-29 12:53:51 +0200

    run_test.py

--- run_test.py
+++ run_test.py
@@ -1,3 +1,5 @@
+
+
 # -*- coding: utf-8 -*-
 #-----------------------------------------------------------------------
 #        This script takes a rule base R
@@ -17,15 +19,21 @@
 from splitR import *
 from optimum_partition_for_Q import *
 
+def print_list(some_list):
+    for i in some_list:
+        print(i)
 
-# Consider the following rule base:
+
+# Consider the following rule base: (Add here the rule base for the proves)
+R = [( (1,2,3,8,11), (4,6), 'A'), (5, 4, 'B'), ( (9,12), 5, 'C')]
+"""
 R = [
 #connected 1
 ( (1, 2, 3, 8, 11), (4, 6), 'A'),
 (     5,             4,     'B'),
 ( (9,12),            5,     'C'),
-#connected 2
 
+#connected 2
 ( (2,5),             7,     'D'),
 
 #connected 3
@@ -34,17 +42,20 @@ R = [
 #connected 4
 ( (12),            (10,13), 'B'),
 ( (11,13),          (11,13),'D'),
+
 #connected 5
 (   8,             (10,14), 'A'),
 (  (6,9),            11,    'A')
 ]
+"""
 
-
-print('Rules', R)
+print('Rules test 1')
+print_list(R)
 print('--------------------------------------------------')
 # Separate R into its connected components
 all_connected_sets = extract_connected_sets( R )
-print('All connected sets : ', all_connected_sets)
+print('All connected sets : ')
+print_list(all_connected_sets)
 #Separate the connected components into lonly rules and connected components
 #with more than one rule
 [ lonly_rules, connected_rules ] = connected_and_lonly_rules( all_connected_sets )
@@ -56,5 +67,5 @@ print('Set of rules from which inference rules can be derived: ')
 for rule in rules:
     print(rule)
 #Check that there are no intersections among the resulting rules
-#adjacent_matrix( rules )
+adjacent_matrix( rules )
 
