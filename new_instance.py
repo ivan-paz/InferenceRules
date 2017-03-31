4c38c98ead8daad9bcd971cef112f0b764d8fd24
ivan-paz <ivnpaz@gmail.com>
2017-03-31 17:33:06 +0200

    test new processing pattern

--- new_instance.py
+++ new_instance.py
@@ -1,21 +1,30 @@
 #---------------------------------------------------------------
 #
-#   This script recives a new instance and modify accordingly the existent rule base
+#   This script contains the functions to
+#   receive a new pattern and modify
+#   according the existent rule base.
+#
+#   To avoid unnecessary processing,
+#   the function " intersected_connected_sets "
+#   find which of the original connected sets
+#   are intersected by the new pattern.
+#   
+#  Those connected sets together with the new pattern
+#  form a " new (connected) set" which will be cut
+#  following the maximum volume heuristic
+#
+#  The resulting rule base is build with the partition
+#  of the new connected_set + the partitions comming from
+#  the not_intersected connected sets.
 #
 #----------------------------------------------------------------
-
-#  When a new instance "new_instance" comes,
-#  in order to adjust only what is necessary we proceed as follows:
-
-#  1. Find which of the original connected sets are intersected by the new instance.
-#  2. Separate those sets and keep the rules that come from the others.
 
 import json
 from splitR_version1 import *  #  CURRENT VERSION  #
 from optimum_partition_for_Q import *
 
 
-# Function to read a json file ---------------------
+# Read a json file ---------------------
 def read(file_name):
     with open(file_name) as json_data:
         file_content = json.load(json_data)
@@ -24,12 +33,12 @@ def read(file_name):
 # Read files with all_connected_sets and their indexes
 all_connected_sets = read('all_connected_sets.json')
 #indexes_all_connected_sets = read('indexes_connected_sets.json')
-
 #for i in all_connected_sets: print(i)
+
 
 # Find wich of the original connected_sets are intersected by the new instance
 # get its indexes
-def new( new_pattern, all_connected_sets ):
+def intersected_connected_sets( new_pattern, all_connected_sets ):
     indexes_of_intersected_sets = [ ]
     intersected_sets = []
     index_counter = -1
@@ -37,12 +46,12 @@ def new( new_pattern, all_connected_sets ):
         index_counter += 1
         include_set = False
         for rule in connected_set:
-            print(new_pattern, rule)
+            #print(new_pattern, rule)
             intersects = intersection(new_pattern, rule)
-            print(intersects)
+            #print(intersects)
             if intersects == True:
                 include_set = True
-                print(index_counter)
+                #print(index_counter)
         if include_set == True:
             indexes_of_intersected_sets.append(index_counter)
             intersected_sets.append(connected_set)
@@ -50,9 +59,9 @@ def new( new_pattern, all_connected_sets ):
 
 
 #   NEW INSTANCE 
-pattern = ( 2, 5, 'A' )
+#pattern = ( 2, 5, 'A' )
 
-[ intersected_sets,  indexes_of_intersected_sets ] = new( pattern, all_connected_sets)
+#[ intersected_sets,  indexes_of_intersected_sets ] = intersected_connected_sets( pattern, all_connected_sets)
 
 #Create new set with the new pattern + its intersections
 def pattern_plus_intersections(pattern, intersected_sets):
@@ -63,10 +72,15 @@ def pattern_plus_intersections(pattern, intersected_sets):
         new_set.append(pattern)
         return new_set
 
-print(pattern_plus_intersections(pattern, intersected_sets) )
+#print('This is the set to cut : ' )
+#new_set = pattern_plus_intersections(pattern, intersected_sets)
+#print(new_set)
+
+#print('This is the set without inconsistencies among the rules: ')
+#new_set = optimum_partition( new_set )
+#print(new_set)
 
 #------------------------------------------------------------------------------------
-#
 #                           Exclude from optimim partitions
 #                           the indexes_of_intersected_sets
 #                           keep the rest of the partitions
@@ -76,13 +90,76 @@ optimum_partitions_indexes = read('connected_rules_indexes.json')  #  They share
 lonly_rules = read('lonly_rules.json')
 lonly_rules_indexes = read('lonly_rules_indexes.json')
 
-#  compare the indexes of optimum partitions (partitions for the connected rules)  and lonly rules
-#  with the indexes of the intersected sets
-#  if they match eliminate the entrance either in optimim_partitions of in lonly_rules
-#  return a final set without the excluded sets
-def keep_partitons(optimum_partitions, optimum_partitions_indexes, lonly_rules, lonly_rules_indexes, indexes_of_intersected_sets):
-    keep = []
+#print('------------------')
+#print('indexes of intersected sets :  ' , indexes_of_intersected_sets)
+#print('optimum_partitions_indexes',optimum_partitions_indexes)
+#print('lonly_rules_indexes', lonly_rules_indexes)
 
+
+#  compare the indexes of optimum partitions (partitions for the connected rules) and lonly rules
+#  with the indexes of the intersected sets
+#  if they match, eliminate the entrance either in the optimim_partitions of in the lonly_rules
+#  return as final set {lonly_rules, optimum_partitions}\{elements_at_indexes_of_intersected_rules}
+def remaining_partitions(optimum_partitions, optimum_partitions_indexes, lonly_rules, lonly_rules_indexes, indexes_of_intersected_sets):
+    kept_partitions = []
+    kept_lonly = []
+
+    for i in range( len(optimum_partitions_indexes) ):
+        index = optimum_partitions_indexes[i]
+        flag = index in indexes_of_intersected_sets
+        if flag == False:
+            kept_partitions = kept_partitions + optimum_partitions[i]
+
+    for i in range( len(lonly_rules_indexes) ):
+        index = lonly_rules_indexes[i]
+        flag = index in indexes_of_intersected_sets
+        if flag == False:
+            kept_lonly.append(lonly_rules[i])
+    return [ kept_partitions, kept_lonly]
+
+#print( 'REMAINING PARTITIONS : ')
+
+#not_intersected = remaining_partitions(optimum_partitions, optimum_partitions_indexes, lonly_rules, lonly_rules_indexes, indexes_of_intersected_sets)
+#print(not_intersected)
+
+#---------------------------------------------------------------
+#    The resulting rule base incorporating the new parrern
+#    is the set { remaining_partitions U new_set }
+#
+#---------------------------------------------------------------
+def not_intersected_union_new_set(not_intersected, new_set):
+    union = []
+    for i in not_intersected:
+        for j in i: union = union + j
+    for i in new_set:
+        union.append(i)
+    return union
+#print('result: ')
+#print( not_intersected_union_new_set(not_intersected, new_set) )
+
+#-----------------------------------------------------------------
+#
+#           PROCESS  ---   NEW  ---  PATTERN
+#
+#-----------------------------------------------------------------
+def process_new_pattern(pattern):
+    all_connected_sets = read('all_connected_sets.json')
+    [intersected_sets, indexes_of_intersected_sets] = intersected_connected_sets(pattern, all_connected_sets)
+    new_set = pattern_plus_intersections(pattern,intersected_sets)
+    new_set = optimum_partition( new_set)
+    
+    optimum_partitions = read('optimum_partitions.json')
+    optimum_partitions_indexes = read('connected_rules_indexes.json')
+    lonly_rules = read('lonly_rules.json')
+    lonly_rules_indexes = read('lonly_rules_indexes.json')
+
+    not_intersected = remaining_partitions(optimum_partitions, optimum_partitions_indexes, lonly_rules, lonly_rules_indexes,indexes_of_intersected_sets)
+    
+    new_rule_base = not_intersected_union_new_set(not_intersected,new_set)
+    return new_rule_base
+
+#  pattern = ( 2,  5,  'A' )
+#  print('new rule base: ', process_new_pattern(pattern))
 
 
 
---
+++ test_new_pattern.py
@@ -0,0 +1,16 @@
+#
+#
+#
+#
+#
+
+from new_instance import *
+
+
+#pattern = ( 2, 5, 'A')
+
+# when processing new patterns first call rulex and then break
+#pattern = ( 5, 5, 'B')
+
+new_rule_base = process_new_pattern( pattern )
+print(new_rule_base)
